dec7164fc5b65c66029a7d13da7bec3809e7966c
Dmitry Ivanov <ivadmi5@gmail.com>
2024-05-30 18:58:31 +0300

    add tokio-tracing

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,56 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use opentelemetry::{
-    global::{self, ObjectSafeSpan},
-    trace::{SpanKind, Status, Tracer},
+use opentelemetry::KeyValue;
+use opentelemetry_sdk::resource::Resource;
+use opentelemetry_semantic_conventions::{
+    resource::{DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION},
+    SCHEMA_URL,
 };
-use opentelemetry_otlp::WithExportConfig;
-use opentelemetry_sdk::trace::TracerProvider;
 use rand::Rng;
 use std::{convert::Infallible, net::SocketAddr};
 
-fn init_tracing() -> anyhow::Result<()> {
-    let span_exporter = opentelemetry_otlp::new_exporter()
-        .tonic()
-        .with_endpoint("http://localhost:4317")
-        .build_span_exporter()?;
+use tracing_core::Level;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-    let provider = TracerProvider::builder()
-        .with_config(opentelemetry_sdk::trace::config().with_resource(opentelemetry_sdk::Resource::new(vec![
-            opentelemetry::KeyValue {
-                key: opentelemetry_semantic_conventions::resource::SERVICE_NAME.into(),
-                value: "demo_server".into(),
-            },
-        ])))
-        .with_batch_exporter(span_exporter, opentelemetry_sdk::runtime::Tokio)
-        .build();
+// Create a Resource that captures information about the entity for which telemetry is recorded.
+fn resource() -> Resource {
+    Resource::from_schema_url(
+        [
+            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+            KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+            KeyValue::new(DEPLOYMENT_ENVIRONMENT, "develop"),
+        ],
+        SCHEMA_URL,
+    )
+}
 
-    global::set_tracer_provider(provider);
+// Construct Tracer for OpenTelemetryLayer
+fn init_tracer() -> anyhow::Result<opentelemetry_sdk::trace::Tracer> {
+    let pipeline = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_trace_config(opentelemetry_sdk::trace::Config::default().with_resource(resource()))
+        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
+        .install_batch(opentelemetry_sdk::runtime::Tokio)?;
+
+    Ok(pipeline)
+}
+
+fn init_tracing_subscriber() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::filter::LevelFilter::from_level(
+            Level::INFO,
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .with(OpenTelemetryLayer::new(init_tracer()?))
+        .init();
 
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    init_tracing()?;
+    init_tracing_subscriber()?;
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
@@ -46,24 +64,24 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+#[tracing::instrument]
+async fn do_work(job: &str) {
+    tokio::time::sleep(std::time::Duration::from_millis(200)).await;
+}
+
+#[tracing::instrument]
 async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    do_work("uno").await;
+    do_work("dos").await;
+
     let mut response = Response::new(Body::empty());
-    let tracer = global::tracer("dice_server");
-
-    let mut span = tracer
-        .span_builder(format!("{} {}", req.method(), req.uri().path()))
-        .with_kind(SpanKind::Server)
-        .start(&tracer);
-
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/rolldice") => {
             let random_number = rand::thread_rng().gen_range(1..7);
             *response.body_mut() = Body::from(format!("{random_number}\n"));
-            span.set_status(Status::Ok);
         }
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
-            span.set_status(Status::error("Not Found"));
         }
     };
 
